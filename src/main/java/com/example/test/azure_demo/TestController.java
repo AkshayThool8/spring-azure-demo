88cd29b4a377f79f875cfd07bc074ae66ba40913
Akshay_Thool <akshay.thool@sunbeltrental.com>
2025-08-06 20:09:56 +0530

    first commit

---
+++ src/main/java/com/example/test/azure_demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.test.azure_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/home")
+    public String testMethod() {
+        return "Application got deployed to azure successfully";
+    }
+}
